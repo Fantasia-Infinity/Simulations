3f5a9f828ed616d003e400b3f9d374de3b08b055
Fantasia-Infinity <1134693876@qq.com>
2025-05-12 16:35:49 +0200

    fix

--- Slime12.py
+++ Slime12.py
@@ -513,8 +513,8 @@ class Ground:
         self.decomposeC4(x, y)
 
     def update(self):
-        l1 = range(self.n)
-        l2 = range(self.n)
+        l1 = list(range(self.n))
+        l2 = list(range(self.n))
         random.shuffle(l1)
         random.shuffle(l2)
         for i in l1:  # 因并不是严格的并行更新 所以打乱更新顺序防止瞬移的bug
